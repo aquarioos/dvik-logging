3872834575d129af66f3077259e2b961c15b7eb1
aquarioos <aquarioos@yandex.com>
2018-06-14 08:53:43 +0200

    sposób użycia, zmiana z md na rst

--- README.rst
+++ README.rst
@@ -1,2 +1,4 @@
-# dvik-logging
+dvik-logging
+------------
+
 Pakiet pisany w ramach projektu DVik. Jest to dodatkowa warstwa na bibliotekę *logging*. Umożliwi łatwe pobranie i skonfigurowanie obiektu *loggera* wraz z odpowiednimi *uchwytami* (*handlerami*).
--- dvik_logging/__init__.py
+++ dvik_logging/__init__.py
@@ -1,12 +1,23 @@
 """
 DVikLogging
-~~~~~~~~~~~~
+~~~~~~~~~~~
 
 Pakiet pisany w ramach projektu DVik. Jest to dodatkowa warstwa na bibliotekę logging. Umożliwi łatwe pobranie i skonfigurowanie obiektu loggera wraz z odpowiednimi uchwytami (handlerami).
 
-Przykładowe użycia:
+Użycie:
 
-TODO
+    ch = get_console_handler(level=LEVELS.DEBUG, formatter=FORMATTERS.TIME_ONLY, dt_formatter=DT_FORMATTERS.SHORT)
+    fh = get_file_handler(log_file_path, log_level=LEVELS.INFO, formatter=FORMATTERS.CLASSIC,
+            dt_formatter=DT_FORMATTERS.CLASSIC)
+
+    L = get_logger('nazwa', console_handler=ch, file_handler=fh)
+
+    ...
+
+    L.debug('debugowanie')
+    L.info('informacja')
+    L.warning('ostrzezenie')
+    L.error('błąd')
 """
 
 from .dvik_logging import get_logger
